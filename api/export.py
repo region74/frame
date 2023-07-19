7e176c4c497e7f513bfe2f5e74a3ffb479ee5974
Кирилл <kiryamiass7@gmail.com>
2023-07-19 15:14:17 +0500

    api sipuni

--- api/export.py
+++ api/export.py
@@ -1,67 +1,27 @@
+from datetime import datetime
 import requests
-import hashlib
-from datetime import date
-import urllib.parse
+from api.config import data
+import asyncio
 
-user = '051340'
-from_date = date.today().strftime("%d.%m.%Y")
-to_date = date.today().strftime("%d.%m.%Y")
-type = '2'
-state = '0'
-tree = '000691234'
-showTreeId = '0'
-fromNumber = ''
-toNumber = ''
-numbersRinged = 0
-numbersInvolved = 1
-names = 1
-outgoingLine = 1
-toAnswer = ''
-anonymous = '1'
-firstTime = '0'
-dtmfUserAnswer = '0'
-hash = '0.njnaxjoq79'
 
-hash_string = '+'.join(
-    [anonymous, str(dtmfUserAnswer), firstTime, from_date, fromNumber, str(names), str(numbersInvolved),
-     str(numbersRinged), str(outgoingLine), showTreeId, state, to_date, toAnswer, toNumber, tree, type, user, hash]
-)
+# функция обращается по api и получает csv файл
+async def get_data():
+    api_url = 'https://sipuni.com/api/statistic/export'
+    try:
+        response = requests.post(api_url, data=data)
+        if response.status_code == 200:
+            with open(f"data.csv", 'wb') as file:
+                file.write(response.content)
+        else:
+            with open('api.log', 'a', encoding='utf-8') as file:
+                file.write(f'{datetime.now()} {response.status_code}\n')
+    except Exception as e:
+        with open('api.log', 'a', encoding='utf-8') as file:
+            file.write(f'{datetime.now()} {str(e)}\n')
 
-hash = hashlib.md5(hash_string.encode()).hexdigest()
 
-url = 'https://sipuni.com/api/statistic/export'
-data = {
-    'anonymous': anonymous,
-    'dtmfUserAnswer': dtmfUserAnswer,
-    'firstTime': firstTime,
-    'from': from_date,
-    'fromNumber': fromNumber,
-    'names': names,
-    'numbersInvolved': numbersInvolved,
-    'numbersRinged': numbersRinged,
-    'outgoingLine': outgoingLine,
-    'showTreeId': showTreeId,
-    'state': state,
-    'to': to_date,
-    'toAnswer': toAnswer,
-    'toNumber': toNumber,
-    'tree': tree,
-    'type': type,
-    'user': user,
-    'hash': hash,
-}
+def main():
+    asyncio.run(get_data())
 
-response = requests.post(url, data=data)
-print(response.status_code)
 
-filename = f"stat_{from_date}-{to_date}.csv"
-content_disposition = f"attachment; filename={filename}"
-response_headers = {'Content-Disposition': content_disposition}
-
-# Формируем URL-строку с GET-параметрами
-url_with_params = 'https://sipuni.com/api/statistic/export?' + urllib.parse.urlencode(data)
-print("Полный URL с параметрами:")
-print(url_with_params)
-
-with open(filename, 'wb') as file:
-    file.write(response.content)
+main()
