adfab135dc6d04e687cca1dabddd2bea26a0a3f3
Кирилл <kiryamiass7@gmail.com>
2023-07-20 16:41:40 +0500

    pandas tools

--- tools.py
+++ tools.py
@@ -46,7 +46,12 @@ def table_opener():
 
 # По времени дня
 def table_time_day():
-    pass
+    data['Время'] = pd.to_datetime(data['Время'], format='%d.%m.%Y %H:%M:%S')
+    data['Часы'] = data['Время'].dt.hour
+    table = data.pivot_table(index='Часы', values=['Звонок', 'Дозвон'], aggfunc=[np.sum])
+    table['Дозвон%'] = table[('sum', 'Дозвон')] / table[('sum', 'Звонок')]
+    table = table.drop(columns=('sum', 'Дозвон'))
+    print(table)
 
 
 # Опенер-телефон
@@ -58,7 +63,8 @@ def table_opener_number(options: Union[int, str]):
         # Убираю лишнее поле
         table = table.drop(columns=('sum', 'Дозвон'))
         table2 = table.pivot_table(index='Откуда', columns='Исходящая линия', fill_value=0)
-        table2.to_csv('tmp.csv', index=True)
+        print(table2)
+        # table2.to_csv('tmp.csv', index=True)
     # разрез дозвонов
     elif options == 2:
         # добавляем в нее вычисляемое поле
@@ -67,13 +73,12 @@ def table_opener_number(options: Union[int, str]):
         table = table.drop(columns=('sum', 'Звонок'))
         # сводная от сводной, чтобы имея проценты разбить по номерам в столбцах
         table2 = table.pivot_table(index='Откуда', columns='Исходящая линия', fill_value=0)
-        table2.to_csv('tmp.csv', index=True)
+        print(table2)
+        # table2.to_csv('tmp.csv', index=True)
 
     # Аналог создания первой сводной, только иными инструментами
     # table = data.groupby(['Откуда', 'Исходящая линия']).agg({'Звонок': np.sum, 'Дозвон': np.sum})
     # table['%дозвон']=table['Дозвон'] / table['Звонок']
-
-    # print(table)
 
 
 # Опенер-время
@@ -81,5 +86,6 @@ def table_opener_time():
     pass
 
 
-table_opener_number(2)
+table_time_day()
+# table_opener_number(2)
 # table_opener()
