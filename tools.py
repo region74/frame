1e29c6f4807a1aefc2c031c04b9b9b1024979794
Кирилл <kiryamiass7@gmail.com>
2023-07-20 12:25:21 +0500

    pandas tools

--- tools.py
+++ tools.py
@@ -49,13 +49,20 @@ def table_time_day():
 
 # Опенер-телефон
 def table_opener_number():
-    table = data.pivot_table(index=['Откуда'], columns='Исходящая линия',values=['Звонок','Дозвон'],
-                             aggfunc=[np.sum])
-    # ДИЧЬ ПОЛНАЯ
+    table = data.pivot_table(index=['Откуда', 'Исходящая линия'], values=['Звонок', 'Дозвон'],
+                             aggfunc=[np.sum], fill_value=0)
 
     # добавляем в нее вычисляемое поле
-    # table['Дозвон%'] = table[('sum', 'Дозвон')] / table[('sum', 'Звонок')]
+    table['Дозвон%'] = table[('sum', 'Дозвон')] / table[('sum', 'Звонок')]
+    # Убираю лишнее поле
+    table = table.drop(columns=('sum', 'Дозвон'))
+    # сводная от сводной, чтобы имея проценты разбить по номерам в столбцах
+    table2 = table.pivot_table(index='Откуда', columns='Исходящая линия', fill_value=0)
+    # чтобы глянуть в живую ч там
+    table2.to_csv('tmp.csv', index=True)
 
+
+    # Аналог создания первой сводной, только иными инструментами
     # table = data.groupby(['Откуда', 'Исходящая линия']).agg({'Звонок': np.sum, 'Дозвон': np.sum})
     # table['%дозвон']=table['Дозвон'] / table['Звонок']
 
@@ -65,6 +72,7 @@ def table_opener_number():
 # Опенер-время
 def table_opener_time():
     pass
+
 
 table_opener_number()
 # table_opener()
